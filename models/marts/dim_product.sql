03c7b51a225dd48c3949b09b5abd7afc05a693ca
brbiancr <brbiancr@gmail.com>
2026-08-28 13:44:09 -0300

    correcao de erro em dim_product

--- models/marts/dim_product.sql
+++ models/marts/dim_product.sql
@@ -6,6 +6,6 @@ select
     -- Cria a surrogate key aplicando hash MD5 no product_id
     md5(cast(product_id as varchar)) as product_key,
 
-    product_id
+    product_id,
     product_category_name
 from products
